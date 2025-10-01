afc425778f205b7b99558a5e6418add30c3f73b0
3bdulla03 <160263095+3bdulla03@users.noreply.github.com>
2025-10-01 09:46:17 +0300

    Add event listeners for start and how to play buttons

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
+const startBtn = document.getElementById("startBtn")
+const howToPlay = document.getElementById("howToPlay")
+
+startBtn.addEventListener("click", function(){
+    window.location.href = "board.html" 
+    }
+)   
+
+howToPlay.addEventListener("click", function(){
+    window.location.href = "howToPlay.html"
+})
+
+
+
 
