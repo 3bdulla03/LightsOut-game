47a3ca8e51cf703cf83b469903cfb003d2e8d879
3bdulla03 <160263095+3bdulla03@users.noreply.github.com>
2025-10-01 09:48:29 +0300

    Add navigation functionality to howToPlay.js

---
+++ howToPlay.js
@@ -0,0 +1,9 @@
+const playBtn = document.getElementById("startBtn")
+const homeBtn = document.getElementById("homeBtn")
+
+playBtn.addEventListener("click", function(){
+    window.location.href = "board.html"
+})
+homeBtn.addEventListener("click", function(){
+    window.location.href = "home.html"
+})
